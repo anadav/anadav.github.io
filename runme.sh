1fea0fbc4836f6fa3414933775b86b5534047b19
Nadav Amit <nadav.amit@gmail.com>
2018-10-16 17:05:01 -0700

    scripts

---
+++ runme.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# step 1
+git branch -D master
+git checkout -b master master-empty
+git push --set-upstream origin master -f
+git checkout sources
+
+# step 2
+cd blog
+bundle exec jekyll serve
+cd ..
+git checkout master
+
+# step 3
+cd blog
+
+echo ".sass-cache" > _site/.gitignore
+echo ".jekyll-metadata" >> _site/.gitignore
+echo ".DS_Store" >> _site/.gitignore
+
+git --work-tree=_site add .gitignore
+git --work-tree=_site add --all
+git --work-tree=_site commit -m 'autogen: update'
+git --work-tree=_site push -f
+git checkout sources
+git reset --hard
