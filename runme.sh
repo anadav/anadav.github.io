7bf4d93d6ba1192a2da1eac63db97a67abe23560
Nadav Amit <nadav.amit@gmail.com>
2023-12-26 12:11:16 +0200

    up

--- runme.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-bundle exec jekyll clean
-bundle exec jekyll build
-echo ".sass-cache" > _site/.gitignore
-echo ".jekyll-metadata" >> _site/.gitignore
-echo ".DS_Store" >> _site/.gitignore
-
